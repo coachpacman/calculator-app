6717ad0eb0bc95e1e3c60c332fd3af3c60a62c7b
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-11 22:25:22 -0700

    initial thoughts on math functions

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-//if number button is clicked, display number in display
-  //add event listener to all buttons
-    //add event listener to all number buttons
-      //add event listener to the number nine
-  //event listener updates display html
 const display = document.getElementById("display");
 
 //Add event listeners to all buttons
@@ -17,6 +12,21 @@ numBtns.forEach(function(btn) {
     display.innerHTML = newNumber
   })
 })
+
+//new number is num 1 + operator + num2
+//num 1 is current value in display
+//math function is value of operator button
+//num 2 is next value in display
+//next operator shows new value
+//case switch statement based on operator selected
+var num1;
+var num2;
+var operator;
+
+function addition(num1, num2) {
+  var newNumber;
+  newNumber = num1 + num2
+}
 
 // document.getElementById("btn-nine").addEventListener("click", function(e) {
 //   var curNumber = display.innerHTML;
