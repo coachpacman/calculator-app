062f569341db76c17b057237c875211be6232c2b
Tim Lee <tim.wenyang.lee@gmail.com>
2023-11-01 10:25:44 -0700

    group code by variables, functions, helper functions

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-//used for performing math functions. I assume all math functions follow form num1, math operator, num2
 let num1 = "";
 let num2 = "";
 let math = "";
 
-function clearDisplay() {
-  num1 = "";
-  num2 = "";
-  math = "";
-  display.innerHTML = "0";
+const operations = {
+  "addition": (a, b) => a + b,
+  "subtraction": (a, b) => a - b,
+  "multiplication": (a, b) => a * b,
+  "division": (a, b) => a / b
 }
 
 const display = document.getElementById("display");
-
-//Number button event listeners. Values will concatenate if a math operand hasn't been identified
 const numBtns = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 
 numBtns.forEach(function(btn) {
   btn.addEventListener("click", function(e) {
@@ -23,6 +21,7 @@ numBtns.forEach(function(btn) {
       //if additional number is clicked, show concatenated number; ex: press 1, press 1, display 11
       num2 = num2.concat(btnNumber);
       display.innerHTML = num2
+      
     } else {
       const btnNumber = e.target.innerHTML;
       num1 = num1.concat(btnNumber);
@@ -30,27 +29,6 @@ numBtns.forEach(function(btn) {
     }
   })
 })
-
-const operations = {
-  "addition": (a, b) => a + b,
-  "subtraction": (a, b) => a - b,
-  "multiplication": (a, b) => a * b,
-  "division": (a, b) => a / b
-}
-
-//Performs current math operation, then updates num1 with new calculated value and clears num2 for next number value
-function calculate(param1, param2) {
-  num1 = Number(param1);
-  num2 = Number(param2);
-
-  newNumber = operations[math](num1, num2);
-  display.innerHTML = newNumber;
-  num1 = newNumber;
-  num2 = "";
-}
-
-//Math operator event listeners. If num2 exists, perform current math operation and then update math operator
-const operators = document.querySelectorAll(".operator");
 
 for (const operator of operators) {
   operator.addEventListener("click", e => {
@@ -61,3 +39,21 @@ for (const operator of operators) {
       math = e.target.id;
     }
   })
+}
+
+function calculate(param1, param2) {
+  num1 = Number(param1);
+  num2 = Number(param2);
+
+  newNumber = operations[math](num1, num2);
+  display.innerHTML = newNumber;
+  num1 = newNumber;
+  num2 = "";
+}
+
+function clearDisplay() {
+  num1 = "";
+  num2 = "";
+  math = "";
+  display.innerHTML = "0";
+}
